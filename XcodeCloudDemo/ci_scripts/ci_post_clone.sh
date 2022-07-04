1a38c2db9f034eb43da5cf076528965852e14657
Artem Kvasnetskyi <artem.kvasnetskyi@chisw.com>
2022-07-04 16:46:26 +0300

    - Add setting app icon to project via script

--- XcodeCloudDemo/ci_scripts/ci_post_clone.sh
+++ XcodeCloudDemo/ci_scripts/ci_post_clone.sh
@@ -9,8 +9,16 @@
 # Install CocoaPods using Homebrew.
 brew install cocoapods
 
-# Install SwiftGen using Homebrew.
-brew install swiftgen
-
 # Install dependencies you manage with CocoaPods.
 pod install
+
+# Set app icon to project
+if [[ -n $CI_XCODE_BUILD_ACTION = 'archive' ]];
+then
+    echo "Setting App Icon"
+    APP_ICON_PATH=$CI_WORKSPACE/Resources/Assets.xcassets/AppIcon.appiconset
+    
+    rm -rf $APP_ICON_PATH
+    
+    mv "$CI_WORKSPACE/ci_scripts/AppIcon.appiconset" $APP_ICON_PATH
+fi
