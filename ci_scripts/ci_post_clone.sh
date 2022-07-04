221b3db066f612d344d321efb06781a3154cbd48
Artem Kvasnetskyi <artem.kvasnetskyi@chisw.com>
2022-07-04 19:56:25 +0300

    - Test post clone script [ci skip]

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,7 +7,24 @@
 #
 
 # Install CocoaPods using Homebrew.
-brew install --cask cocoapods
+# brew install --cask cocoapods
 
 # Install dependencies you manage with CocoaPods.
-pod install
+# pod install
+
+cd ..
+ 
+echo ">>> SETUP ENVIRONMENT"
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+ 
+echo ">>> INSTALL BUNDLER"
+gem install bundler --install-dir $GEM_HOME
+ 
+echo ">>> INSTALL DEPENDENCIES"
+bundle install
+ 
+echo ">>> INSTALL PODS"
+bundle exec pod install
